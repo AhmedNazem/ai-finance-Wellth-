0ec20280f1abec40bc63812928c7fd1b873b696c
AhmedNazem <aahhaammdd55@gmail.com>
2025-01-02 00:16:52 +0300

    building the dashboard page and page not found (404)

---
+++ app/(main)/dashboard/layout.js
@@ -0,0 +1,18 @@
+import { Suspense } from "react";
+import { BarLoader } from "react-spinners";
+import DashboardPage from "./page";
+function DashboardLayout() {
+  return (
+    <div className="px-5">
+      <h1 className="text-6xl font-bold gradient-title mb-5">Dashboard</h1>
+      {/* dashboard page  */}
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
+      >
+        <DashboardPage />
+      </Suspense>
+    </div>
+  );
+}
+
+export default DashboardLayout;
---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,13 @@
+function DashboardPage() {
+  return (
+    <div className="px-5">
+      {/* Budget progress */}
+
+      {/* Overview */}
+
+      {/* Accounts Grid */}
+    </div>
+  );
+}
+
+export default DashboardPage;
