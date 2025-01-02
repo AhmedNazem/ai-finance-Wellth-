3aa38791220f40e5477364a648789ca92ae264bc
AhmedNazem <aahhaammdd55@gmail.com>
2025-01-03 01:24:22 +0300

    render user accounts

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,16 +1,16 @@
+import { getUserAccounts } from "@/actions/dashboard";
 import CreateAccountDrawer from "@/components/Create-account-drawer";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
+import AccountCard from "./_components/AccountCard";
 
-function DashboardPage() {
+async function DashboardPage() {
+  const accounts = await getUserAccounts();
+
   return (
     <div className="px-5">
-      {/* Budget progress */}
-
-      {/* Overview */}
-
       {/* Accounts Grid */}
-      <div className="grid  gap-4  md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         <CreateAccountDrawer>
           <Card className="hover:shadow-md transition-shadow cursor-pointer border-dashed">
             <CardContent className="flex flex-col items-center justify-center text-muted-foreground h-full pt-5">
@@ -19,6 +19,10 @@ function DashboardPage() {
             </CardContent>
           </Card>
         </CreateAccountDrawer>
+        {accounts.length > 0 &&
+          accounts.map((account) => {
+            return <AccountCard key={account.id} account={account} />;
+          })}
       </div>
     </div>
   );
