85c7a9f4afeb090664eef32c4e1f28483829a2a5
AhmedNazem <aahhaammdd55@gmail.com>
2025-01-10 02:02:43 +0300

    building the dashboard charts

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -66,3 +66,21 @@ export async function getUserAccounts() {
   const serializedAccounts = accounts.map(serializeTransaction);
   return serializedAccounts;
 }
+
+export async function getDashboardData() {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+  const user = await db.user.findUnique({
+    where: {
+      clerkUserId: userId,
+    },
+  });
+  if (!user) {
+    throw new Error("User not found");
+  }
+  const transactions = await db.transaction.findMany({
+    where: { userId: user.id },
+    orderBy: { date: "desc" },
+  });
+  return transactions.map(serializeTransaction);
+}
--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,10 +1,12 @@
-import { getUserAccounts } from "@/actions/dashboard";
+import { getDashboardData, getUserAccounts } from "@/actions/dashboard";
 import CreateAccountDrawer from "@/components/Create-account-drawer";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
 import AccountCard from "./_components/AccountCard";
 import { getCurrentBudget } from "@/actions/budget";
 import BudgetProgress from "./_components/BudgetProgress";
+import { Suspense } from "react";
+import DashboardOverview from "./_components/DashboardOverview";
 
 async function DashboardPage() {
   const accounts = await getUserAccounts();
@@ -13,6 +15,7 @@ async function DashboardPage() {
   if (defaultAccount) {
     budgetData = await getCurrentBudget(defaultAccount.id);
   }
+  const transactions = await getDashboardData();
   return (
     <div className="space-y-8">
       {/* Budget Progress */}
@@ -23,6 +26,12 @@ async function DashboardPage() {
         />
       )}
       {/* Overview */}
+      <Suspense fallback={"Loading Overview ..."}>
+        <DashboardOverview
+          accounts={accounts}
+          transactions={transactions || []}
+        />
+      </Suspense>
       {/* Accounts Grid */}
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         <CreateAccountDrawer>
